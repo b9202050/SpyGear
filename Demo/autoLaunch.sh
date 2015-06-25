7475f824f85354b5a07a4e0c635d12faa9f9563e
Danny.H <danny@sentri.me>
2015-06-25 23:36:51 +0800

    update autoLaunch.sh
    Due to we need run autoLaunch.sh when boot, so modify path to absolute
    path in /etc/rc.local.

--- Demo/autoLaunch.sh
+++ Demo/autoLaunch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # start web cam via mjpg_streamer in background
-pushd ~/mjpg-streamer
+pushd /home/pi/mjpg-streamer
 	./mjpg_streamer -i "./input_uvc.so -y -r QVGA -f 15" -o "./output_http.so -w ./www" &
 popd
 
 # start SpyGearPi in background
-pushd ~/dist
+pushd /home/pi/dist
 	sudo java -jar SpyGearPi.jar &
 popd
 
